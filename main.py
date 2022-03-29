2a178ac092ef5ac9a44dbcba435fb6433cbaca53
koval77 <wojtek78@yahoo.com>
2022-03-29 01:18:17 +0100

    Second Commit

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ def print_hi(name):
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 print('Hello')
+print('world')
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
